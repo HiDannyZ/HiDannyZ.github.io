dfe7042dd2f8a5bc2381606b4263f7c6dfbc2576
Danny Zhu <chdannyzhu@yahoo.com>
2023-04-14 14:09:10 -0400

    Added a Box

---
+++ src/declaration.d.ts
@@ -0,0 +1,3 @@
+// Allows importing of JPG and JPEG files to modules
+declare module '*.jpg';
+declare module '*.jpeg';
---
+++ src/pages/indexWave.tsx
@@ -0,0 +1,18 @@
+import * as React from "react"
+import type { HeadFC, PageProps } from "gatsby"
+import '../styles/styles2.css'
+
+const IndexPage: React.FC<PageProps> = () => {
+  return (
+    <div>
+      <div className="wave"></div>
+      <div className="wave"></div>
+      <div className="wave"></div>
+    </div>
+
+  )
+}
+
+export default IndexPage
+
+export const Head: HeadFC = () => <title>Home Page</title>
