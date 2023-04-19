0af4c6704fffed6635442a897ef27c79a84a6fb8
Danny Zhu <chdannyzhu@yahoo.com>
2023-04-19 17:31:23 -0400

    Added SCSS support and Inital construction of  the website

--- src/pages/indexWave.tsx
+++ src/pages/indexWave.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
-import '../styles/styles2.css'
+import '../styles/styles.css'
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
