adf028b7f1007a1ad5c82ec0b610842c5f41172a
Danny Zhu <chdannyzhu@yahoo.com>
2023-05-05 14:51:31 -0400

    Added more stars and refactored some icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,78 +3,207 @@ import type { HeadFC, PageProps } from "gatsby"
 import '../styles/styles.css'
 import '../styles/stylesStars.scss'
 import image from '../assets/illustrations/best.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <div>
-      <div className="background">
-        <div className="stars">
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-                  <div className="star"></div>
-        </div>
-        <div className="navBar">
-          <li><a href="#About">About</a></li>
-          <li><a href="#Projects">Projects</a></li>
-          <li><a href="#Thoughts">Thoughts</a></li>
-          <li><a href="#Experimental">Experimental</a></li>
-        </div>
-        <div className="block glow">            
-          <img src={image} />
-          <h1>About</h1>
-          <h4> Hey, I am Danny Zhu, a software engineer currently based in "NYC Icon" NYC and work at "JPMC Icon" JPM as a Software Engineer</h4>
-          <h4> Outside of programming, you will find me enjoying new foods, traveling, music, anime, gaming, and in general exploring new things.</h4>
+      <div className="navBar">
+        <li><a href="#About">About</a></li>
+        <li><a href="https://github.com/HiDannyZ">Github</a> <FontAwesomeIcon icon="fa-brands fa-github" /></li>
+        <li><a href="https://www.linkedin.com/in/hidannyzhu/">Linkedln</a> <FontAwesomeIcon icon="fa-brands fa-linkedin" /></li>
+        <li><a href="#Experimental">Experimental</a></li>
+        
+        
+      </div>
 
-          <h4> Find me on Github or Linkedln  </h4>
-          <h4> Mail me at chdannyzhu@gmail.com</h4>
-        </div>
+      <div className="stars">
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+      </div>
+      <div className="starsLeftSide">
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+      </div>
+      <div className="block glow">            
+        <img src={image} />
+        <h1>About</h1>
+        <p> Hey, I am Danny Zhu, a software engineer currently based in "NYC Icon" NYC and work at "JPMC Icon" JPM as a Software Engineer</p>
+        <p> Outside of programming, you will find me enjoying new foods, traveling, music, anime, gaming, and in general exploring new things.</p>
+        <p> Email me at chdannyzhu@gmail.com</p>
       </div>
     </div>
   )
